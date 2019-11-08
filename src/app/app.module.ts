55ed598bc656966812b09b5dcde3b0bed2b22792
JulianaCristina <julianacristina780@gmail.com>
2019-11-07 23:08:33 -0300

    Event - exemplo 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaiComponentComponent } from './pai-component/pai-component.component';
+import { FilhoComponent } from './filho/filho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiComponentComponent,
+    FilhoComponent
   ],
   imports: [
     BrowserModule
